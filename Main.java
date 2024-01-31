7cef8742c9f1f6081a2e2e01cc118810b128257b
PranavGarud80 <113354510+PranavGarud80@users.noreply.github.com>
2024-01-31 12:13:22 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,99 +1,55 @@
-
 import java.util.Scanner;
 
-class BankAccount{
-    private int balance;
-
-    public BankAccount(int initialAmount){
-        this.balance = initialAmount;
-    }
-
-    public int balance(){
-        return balance;
-    }
-
-    public void deposit(int amount){
-        if (amount > 0){
-            balance += amount;
-            System.out.println("Deposit of " + amount + "Rs was successfull");
-        } else {
-            System.out.println("Invalid entry!. Please choose a number above 0");
-        }
-    }
-
-    public void withdraw(int amount){
-        if (amount > 0 && amount <= balance){
-            System.out.println("Withdraw successfull of "+amount+"Rs");
-            amount -= balance;
-        } else {
-            System.out.println("Invaid entry!!.Insuffucient balance or negative value entered");
+class Grade{
+    public String grade(int avgp){
+        switch (avgp / 10) {
+            case 10:
+                return "A+";
+            case 9:
+                return "A";
+            case 8:
+                return "B+";
+            case 7:
+                return "B";
+            case 6:
+                return "C";
+            case 5:
+                return "D";
+            default:
+                return "F";
         }
     }
 }
 
-class ATM{
-
-    private BankAccount account; 
-    public ATM(BankAccount account){
-        this.account = account;
-    }
-
-    public void display(){
-        System.out.println("Select Options");
-        System.out.println("1. Deposit");
-        System.out.println("2. Withdraw");
-        System.out.println("3. Check Balance");
-        System.out.println("4. Leave");
-    }
-
-    public void run(){
+public class Main{
+    public static void main(String[] args){
+        Grade g = new Grade();
         Scanner sc = new Scanner(System.in);
-        int option;
-        do{
-            display();
-            System.out.println("Pick an option");
-            option = sc.nextInt();
-            
-            switch (option){
-                case 1:
-                    System.out.println("Enter amount to deposit");
-                    int depositAmount = sc.nextInt();
-                    account.deposit(depositAmount);
-                    break;
-                case 2:
-                    System.out.println("Enter amount to withdraw");
-                    int withdrawAmount = sc.nextInt();
-                    account.withdraw(withdrawAmount);
-                    break;
-                case 3:
-                    System.out.println("Current balance " + account.balance());
-                    break;
-                case 4:
-                    System.out.println("Thanks for working with Jain Bank");
-                    break;
-                default:
-                    System.out.println("Invalid entry!!. Enter only the given options");   
-            }
-        }while (option != 4);
-        sc.close();;
-    }
-}
 
-public class Main {
+        //Taking input of how many subjects are there
+        System.out.println("Enter the amount of subjects");
+        int tsubs = sc.nextInt();
+        int tmarks = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BankAccount userAccount = new BankAccount(30000);
-        ATM atm = new ATM(userAccount);
-        System.out.println("Enter Your PIN");
-        String pin = sc.nextLine();
-        if (pin == "1234") {
-            atm.run();
-        } else {
-            System.out.println("Wrong pin");
+        //Taking and adding all the marks
+        for (int i = 0; i < tsubs; i++) {
+            System.out.println("Enter the marks obtained in subject " + (i + 1));
+            int marks = sc.nextInt();
+            tmarks += marks;
         }
 
-        //lets test the code
+        //Calculating Percentage
+        int avgp = tmarks / tsubs;
 
+        //Calculating Grades
+        String Grade = g.grade(avgp);
+
+        //Displaying all the Data
+        System.out.println("Total marks obtained :- " + tmarks);
+        System.out.println("Percentage obtained :- " + avgp+"%");
+        System.out.println("Grade obtained :- " + Grade);
+
+        sc.close();
+        //Lets test the code
     }
 }
