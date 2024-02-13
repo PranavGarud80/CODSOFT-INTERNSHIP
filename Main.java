2ea7fd4855f8030e8ef3fd1ac2faf5a4531ab902
PranavGarud80 <113354510+PranavGarud80@users.noreply.github.com>
2024-02-13 09:28:11 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,81 @@
+
 import java.util.Scanner;
 
-class rangen{
-    public int genrate(int max,int min){
-        return (int) (Math.random()*(max - min + 1) + min);
+class BankAccount{
+    private int balance;
+
+    public BankAccount(int initialAmount){
+        this.balance = initialAmount;
+    }
+
+    public int balance(){
+        return balance;
+    }
+
+    public void deposit(int amount){
+        if (amount > 0){
+            balance += amount;
+            System.out.println("Deposit of " + amount + "Rs was successfull");
+        } else {
+            System.out.println("Invalid entry!. Please choose a number above 0");
+        }
+    }
+
+    public void withdraw(int amount){
+        if (amount > 0 && amount <= balance){
+            System.out.println("Withdraw successfull of "+amount+"Rs");
+            amount -= balance;
+        } else {
+            System.out.println("Invaid entry!!.Insuffucient balance or negative value entered");
+        }
+    }
+}
+
+class ATM{
+
+    private BankAccount account; 
+    public ATM(BankAccount account){
+        this.account = account;
+    }
+
+    public void display(){
+        System.out.println("Select Options");
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Check Balance");
+        System.out.println("4. Leave");
+    }
+
+    public void run(){
+        Scanner sc = new Scanner(System.in);
+        int option;
+        do{
+            display();
+            System.out.println("Pick an option");
+            option = sc.nextInt();
+            
+            switch (option){
+                case 1:
+                    System.out.println("Enter amount to deposit");
+                    int depositAmount = sc.nextInt();
+                    account.deposit(depositAmount);
+                    break;
+                case 2:
+                    System.out.println("Enter amount to withdraw");
+                    int withdrawAmount = sc.nextInt();
+                    account.withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    System.out.println("Current balance " + account.balance());
+                    break;
+                case 4:
+                    System.out.println("Thanks for working with Jain Bank");
+                    break;
+                default:
+                    System.out.println("Invalid entry!!. Enter only the given options");   
+            }
+        }while (option != 4);
+        sc.close();;
     }
 }
 
@@ -10,49 +83,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        rangen rg = new rangen();
-        int totalAttempts = 0;
-        int win = 0;
-
-        while (true) {
-            System.out.println("Enter the maximum number");
-            int max = sc.nextInt();
-            System.out.println("Enter the minimum number");
-            int min = sc.nextInt();
-            sc.nextLine();
-
-            int cnum = rg.genrate(max, min);
-            int attempts = 0;
-
-            while (true) {
-                System.out.println("Guess a number between "+max+" and "+min);
-                int gnum = sc.nextInt();
-                attempts++;
-
-                if (gnum > cnum) {
-                    System.out.println("Its Greater");
-                } else if (gnum < cnum){
-                    System.out.println("Its Lower");
-                }else{
-                    System.out.println("Correct Guess");
-                    win++;
-                    break;
-                }
-            }
-            totalAttempts += attempts;
-            System.out.println("Attempts = " + attempts);
-            System.out.println("Wins = " + win);
-
-            double winrate = (double) win / totalAttempts*100;
-            System.out.printf("Your winrate is %.2f%%\n",winrate);
-
-            System.out.println("Do you want to play again (yes/no)");
-            String playAgain = sc.next();
-            if(!playAgain.equalsIgnoreCase("yes")){
-                sc.close();
-                System.exit(0);
-            }
-            sc.nextLine();
+        BankAccount userAccount = new BankAccount(30000);
+        ATM atm = new ATM(userAccount);
+        System.out.println("Enter Your PIN");
+        String pin = sc.nextLine();
+        if (pin == "1234") {
+            atm.run();
+        } else {
+            System.out.println("Wrong pin");
         }
+
+        //lets test the code
+
     }
 }
